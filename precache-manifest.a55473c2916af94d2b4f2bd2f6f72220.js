4452f1b9d201422a96e724a28175e86200326981
smket99 <f20180231@hyderabad.bits-pilani.ac.in>
2020-06-21 17:27:36 +0530

    Updates

---
+++ precache-manifest.a55473c2916af94d2b4f2bd2f6f72220.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d441e156998f32020046338915f09933",
+    "url": "/job-portal/index.html"
+  },
+  {
+    "revision": "8ed6b97aa8f2d8290022",
+    "url": "/job-portal/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "8e3f2b4688ab91ef277a",
+    "url": "/job-portal/static/css/main.8134c401.chunk.css"
+  },
+  {
+    "revision": "8ed6b97aa8f2d8290022",
+    "url": "/job-portal/static/js/2.626fd14b.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/job-portal/static/js/2.626fd14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3f2b4688ab91ef277a",
+    "url": "/job-portal/static/js/main.d39b1f6e.chunk.js"
+  },
+  {
+    "revision": "77b84bc79163acf54be0",
+    "url": "/job-portal/static/js/runtime-main.94aaff0a.js"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/job-portal/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/job-portal/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/job-portal/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/job-portal/static/media/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "71a4c745a43655c37e77d5a99c4feab0",
+    "url": "/job-portal/static/media/homelogo.71a4c745.png"
+  },
+  {
+    "revision": "5716990fcb00e58e1096c484e162db74",
+    "url": "/job-portal/static/media/img.5716990f.png"
+  },
+  {
+    "revision": "bdb998fe655741c67ec56f10b8f11d34",
+    "url": "/job-portal/static/media/img2-u.bdb998fe.jpg"
+  },
+  {
+    "revision": "f01e441ed74b9f56d062d66a051a8063",
+    "url": "/job-portal/static/media/img2.f01e441e.jpg"
+  }
+]);
